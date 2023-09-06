30079da2186d5ce22e4ee45dfee6d7d121f1a49e
Peter <balkrishnaoli>
2023-09-06 07:52:42 +0545

    Added the html for for login and signUp and accepted POST requests

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['GET', 'POST'])
 def logout():
     return "<p> Logout <p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get(('password2'))
+
+        if len(email) < 4:
+            pass
+        elif len(firstName):
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            #add the user details
+
     return render_template("sign_up.html")
