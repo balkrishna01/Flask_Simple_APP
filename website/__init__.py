514d92ed637de883225565a05290051b07286a23
Peter <balkrishnaoli>
2023-09-07 08:11:46 +0545

    database schema models initialized

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlchemy
+db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'randomstringonetwothree'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -16,4 +18,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Database Created!')
