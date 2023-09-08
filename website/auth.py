16683fc2c16d66251d7c2b7ee773b013812994e5
Peter <balkrishnaoli>
2023-09-08 08:04:58 +0545

    added the login feature

--- website/auth.py
+++ website/auth.py
@@ -8,6 +8,17 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect Password or Email!', category='error')
+
     return render_template("login.html")
 
 
@@ -24,8 +35,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get(('password2'))
 
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters', category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exits.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(fname) < 2:
             flash('First Name must be greater that one character.', category='error')
         elif password1 != password2:
