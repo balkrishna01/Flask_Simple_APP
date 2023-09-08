35bcdd2d46253d6bd0300135b08c1a002d892bdb
Peter <balkrishnaoli>
2023-09-08 07:38:36 +0545

    Sign UP the user and store information to the db

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -6,6 +9,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
+
 
 @auth.route('/logout', methods=['GET', 'POST'])
 def logout():
@@ -16,20 +20,24 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        fname = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get(('password2'))
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
-        elif len(firstName) < 2:
+        elif len(fname) < 2:
             flash('First Name must be greater that one character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, fname=fname, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created!.', category='success')
+            return redirect(url_for('views.home'))
             # add the user details
 
     return render_template("sign_up.html")
